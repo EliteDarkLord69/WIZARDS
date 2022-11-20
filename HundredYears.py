45741bd00ac5fed8f90869d853033841aabf8efe
JDog012 <josh1harrington@gmail.com>
2022-11-19 19:03:33 -0700

    Deleting unnecessary files in the repository

--- HundredYears.py
+++
@@ -1,5 +0,0 @@
-name = input("What is your name? ")
-age = input("What is your current age? ")
-hundred_year = str(2022 - int(age) + 100)
-
-print('Hello '+ name +', you will be one hundred in the year '+ hundred_year+'.')
--- README.md
+++
@@ -1,2 +0,0 @@
-# PythonLearning
-I'm going to be working through Python practice problems in an attempt to better understand the coding language.
